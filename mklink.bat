b1761980656a82aca39da929dc641b07f73dc7d9
chameme <chevaux.de.bois.meme+dev@gmail.com>
2016-05-10 18:20:36 +0900

    pauseせずexitするように修正

--- mklink.bat
+++ mklink.bat
@@ -3,5 +3,4 @@ mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
 mklink %HOMEPATH%"\.vim\.dein.toml" %HOMEPATH%"\dotfiles\dein\.dein.toml"
 mklink %HOMEPATH%"\.vim\.dein_lazy.toml" %HOMEPATH%"\dotfiles\dein\.dein_lazy.toml"
-REM exit 0
-pause
+exit 0
